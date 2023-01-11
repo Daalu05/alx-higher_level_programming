e615fad5406995513bf8fdcca78808a777b4893e
Daalu05 <arinzechimdalu05@gmail.com>
2023-01-11 14:48:07 -0800

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-if __name__ "__main__":
-    import add from add_0.py
-    for (a, b) = (1, 2)
-    print("[] + [] = []".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    import add from add_0
+    def add(a, b):
+        a = 1
+        b = 2
+        result = a + b
+    print(result)
